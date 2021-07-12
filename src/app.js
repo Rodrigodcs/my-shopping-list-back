d5a66dc087b07614f296bcb99533424111e8ce8e
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-07-12 15:09:11 -0300

    server running properly

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
 import express from "express";
 import cors from "cors";
-
-import connection from "./database.js";
+import connection from './database.js'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/itens",async (req,res)=>{
+    try{
+        const result = await connection.query(`select * from list`);
+        console.log(result.rows)
+        res.send(result.rows)
+    }catch(e){
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
+app.post("/new", async (req,res) =>{
+    try{
+        const { text } = req.body; 
+
+        await connection.query(`
+            INSERT INTO list
+            (name)
+            VALUES ($1)
+        `,[text])
+        
+        res.sendStatus(201)
+    }catch(e){
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
 
 export default app;
--- src/database.js
+++ src/database.js
@@ -3,11 +3,13 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: "bootcamp_role",
-  password: "senha_super_hiper_ultra_secreta_do_role_do_bootcamp",
+  user: "postgres",
+  password: "123",
   host: "localhost",
   port: 5432,
-  database: "my_shopping_list"
+  database: "shopping_list"
 });
 
-export default connection;
+export default connection; 
+
+
